23078ecc19dbd2086a41fdad74d8444cc6de8e26
nicolagreco <notsecurity@gmail.com>
2014-08-05 20:32:07 -0700

    crowd client now working with example

--- index.js
+++ index.js
@@ -2,48 +2,90 @@ module.exports = CrowdClient
 
 function CrowdClient(opts) {
   this.workers = []
-	// handle opts like credential or id
-	return this
+  this.interface = null;
+  // handle opts like credential or id
+  return this
 }
 
 CrowdClient.prototype.use = function(interface) {
-	this.interface = interface
+  this.interface = interface
 
-	return this
+  return this
 }
 
 CrowdClient.prototype.pipe = function(worker) {
-	this.workers.push(worker)
+  this.workers.push(worker)
 
-	return this
+  return this
 }
 
 CrowdClient.prototype.getWorker = function(id) {
   // For now just return first worker in the list
-  return workers[id || 0]
+  return this.workers[id || 0]
 }
 
-CrowdClient.prototype.get = function() {
-  var _this = this
+CrowdClient.prototype.parseTask = function(raw) {
 
-  this.interface
-    .get()
-      .success(function(task) {
-        _this.getWorker(task, _this.send)
-      })
-      .failure(function(error) {
-        console.log(error)
-      })
+  task = JSON.parse(raw)
+  try {
+    task.data = JSON.parse(task.data)
+  } catch(err) {
+    // it is fine, task data will be the same
+  }
+  task.code = this.eval(task.code)
+
+  return task
 
 }
 
-CrowdClient.prototype.send = function(result) {
-  this.interface
-    .send(data)
-      .success(function(task) {
-        _this.getWorker(task, _this.send)
-      })
-      .failure(function(error) {
-        console.log(error)
-      })
+CrowdClient.prototype.get = function(cb) {
+
+  var client = this
+
+  this.interface.get(function(err, raw) {
+    if (err) throw new Error(err)
+    if (cb) cb(null, client.parseTask(raw))
+  })
+
+  return this
+}
+
+CrowdClient.prototype.send = function(result, cb) {
+  
+  var client = this
+
+  this.interface.send(result, function(err, raw) {
+    if (err) throw new Error(err)
+    if (cb) cb(null, client.parseTask(raw))
+  })
+
+  return this
+}
+
+CrowdClient.prototype.eval = function (code) {
+  var body = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
+  return new Function("data", "next", body)
+}
+
+CrowdClient.prototype.start = function (opts, cb) {
+
+
+  this.get(this.runTask(opts, cb))
+
+  return this
+}
+
+CrowdClient.prototype.runTask = function(opts, cb) {
+  var client = this
+  if (typeof opts == "function") cb = opts
+
+  return function(err, task) {
+    if (opts === true) cb = client.runTask(opts, cb)
+    if (err) throw new Error(err)
+
+    var worker = client.getWorker()
+    worker.start(task, function(err, result) {
+      client.send(result, cb)
+    })
+  }
 }
